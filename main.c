96a6d2e79398560f69fa24769d20e8a073f91c66
Andrés Hernández Mata <ahernandez@bitfarm.com.mx>
2021-04-22 02:00:43 +0000

    Actualizar main

--- main.c
+++ main.c
@@ -12,8 +12,7 @@ int main() {
     int i = 0;  
 
     for(i = 0; i < strlen(cadena); i++){        
-        if((cadena[i] == 'a' || cadena[i] == 'e' || cadena[i] == 'i' || cadena[i] == 'o' || cadena[i] == 'u' || cadena[i] == 'A'
-						|| cadena[i] == 'E' || cadena[i] == 'I' || cadena[i] == 'O' || cadena[i] == 'U')){        
+        if((cadena[i] == 'a' || cadena[i] == 'e' || cadena[i] == 'i' || cadena[i] == 'o' || cadena[i] == 'u' || cadena[i] == 'A' || cadena[i] == 'E' || cadena[i] == 'I' || cadena[i] == 'O' || cadena[i] == 'U')){        
             printf("%c\n", cadena[i]); //imprimo las vocales.
         }
     }   
