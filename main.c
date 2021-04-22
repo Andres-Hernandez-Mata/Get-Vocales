59118049c36ad5e4981e4a8980f64258415d734f
Andrés Hernández Mata <ahernandez@bitfarm.com.mx>
2021-04-22 02:02:51 +0000

    Comentarios

--- main.c
+++ main.c
@@ -4,16 +4,18 @@
 int main() {
 
     char cadena[50];
-
-    printf("Ingresar cadena: "); //Se ingresa la cadena
-    scanf("%s", cadena); //Leer la cadena
+		//Se ingresa la cadena
+    printf("Ingresar cadena: "); 
+		//Leer la cadena
+    scanf("%s", cadena); 
 
     //Inicializar la variable
     int i = 0;  
 
     for(i = 0; i < strlen(cadena); i++){        
         if((cadena[i] == 'a' || cadena[i] == 'e' || cadena[i] == 'i' || cadena[i] == 'o' || cadena[i] == 'u' || cadena[i] == 'A' || cadena[i] == 'E' || cadena[i] == 'I' || cadena[i] == 'O' || cadena[i] == 'U')){        
-            printf("%c\n", cadena[i]); //imprimo las vocales.
+            //imprimo las vocales
+						printf("%c\n", cadena[i]); 
         }
     }   
 }
